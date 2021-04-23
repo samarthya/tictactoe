6e9835f0a4f1c1c0f5cbcf1d9fb785f6e4c05324
Saurabh Sharma <saurabh777@gmail.com>
2021-04-23 15:40:39 +0530

    Arrow method

--- src/Square.js
+++ src/Square.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Square extends React.Component {
     render() {
         return (
-            <button className="square" onClick={function () { alert('click'); }}>
+            <button className="square" onClick={() => { alert('click'); }}>
                 {this.props.value}
             </button>
         );
