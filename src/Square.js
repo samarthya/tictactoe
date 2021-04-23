28d34a1984f6d693ff0be577ad1282bd25752135
Saurabh Sharma <saurabh777@gmail.com>
2021-04-23 15:50:22 +0530

    State defined and operation to set X

--- src/Square.js
+++ src/Square.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
 class Square extends React.Component {
+    //React components can have state by setting this.state in their constructors.
+    constructor(props) {
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+        super(props)
+        this.state = {
+            value: null,
+        }
+    }
     render() {
         return (
-            <button className="square" onClick={() => { alert('click'); }}>
-                {this.props.value}
+            <button
+                className="square"
+                onClick={() => { this.setState({ value: 'X' }) }}>
+                {this.state.value}
             </button>
         );
     }
